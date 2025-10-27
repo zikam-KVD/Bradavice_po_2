0171b87447e76bbb406fe85f384d07ccacbc8b01
Miroslav Zíka <mail@kvd.cz>
2025-10-27 17:11:20 +0100

    add jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Models\College;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $koleje = College::all();
+    //dd($koleje);
+
+    return view('welcome', ['colleges' => $koleje]);
 });
 
 Route::view('/video', 'videoHarry')->name("videjko"); 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
